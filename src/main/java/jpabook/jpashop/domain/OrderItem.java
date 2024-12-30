ee3e678de97c5f2739141b97bf215c6bec306fb0
NANOOS105 <heesu105@naver.com>
2024-12-30 22:42:10 +0900

    service repository created

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,7 +2,6 @@ package jpabook.jpashop.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
 
 @Entity
 @Table(name = "order_item")
@@ -12,9 +11,11 @@ public class OrderItem {
     @GeneratedValue
     @Column(name = "order_item_id")
     private Long id;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
-    private AbstractReadWriteAccess.Item item; //주문 상품
+    private Item item; //주문 상품
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order; //주문
