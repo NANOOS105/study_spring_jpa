868807a5ab70d9fad6bd8d950e81b7176cfd9d7a
NANOOS105 <heesu105@naver.com>
2025-01-02 14:43:26 +0900

    Order repository created about Item

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -1,6 +1,6 @@
 package jpabook.jpashop.service;
 import jpabook.jpashop.domain.*;
-import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.domain.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
